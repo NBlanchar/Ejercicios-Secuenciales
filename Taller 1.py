d12eadad4480d1e708b3125c48676284f1cb8f4b
Nefer Blanchar <nblancha1@cuc.edu.co>
2021-09-04 04:30:11 -0500

    Ejercicio 2

--- Taller 1.py
+++ Taller 1.py
@@ -8,3 +8,10 @@ Created on Sat Sep  4 03:48:40 2021
 # Ejercicio 1
 y = ((5 + 2 - 5) ** 2 * 5 + 8 / 2 - 30) / 2 * 5 - 3
 print(y)
+
+# Ejercicio 2
+z = 5
+n = 3
+m = z - n
+y = (((z + 2 - n) ** 2 * m + 8 / 2 - 30) / 2 * 5 - 3) ** 5 + 15 * 3 - 9 / 3
+print(y)
